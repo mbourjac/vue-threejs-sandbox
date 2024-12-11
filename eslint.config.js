7de022f07bb81363889973511adabea307dcab74
Michaël Bourjac <michael.bourjac@gmail.com>
2024-12-11 08:08:56 +0100

    feat: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_', // Ignore variables prefixed with _
+          argsIgnorePattern: '^_', // Ignore arguments prefixed with _
+        },
+      ],
+    },
+  },
+];
