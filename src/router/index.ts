c287fe76528dbb675b78d51832859721299eaa8c
Michaël Bourjac <michael.bourjac@gmail.com>
2024-12-15 09:15:57 +0100

    feat: implement home page layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
+export const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    linkLabel: 'Home',
+  },
+  {
+    path: '/first-scene',
+    name: 'first-scene',
+    component: () => import('../views/FirstSceneView.vue'),
+    linkLabel: 'First Scene',
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  routes: routes.map(({ linkLabel: _, ...rest }) => rest),
 });
 
 export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from '@/router';
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const hoveredLink = ref<string | null>(null);
+</script>
 
 <template>
-  <div>Home</div>
+  <main class="min-h-screen bg-black px-4 text-white">
+    <ul>
+      <template
+        v-for="({ path, name, linkLabel }, index) in routes"
+        :key="name"
+      >
+        <li
+          v-if="name !== 'home'"
+          @mouseenter="hoveredLink = name"
+          @mouseleave="hoveredLink = null"
+          :class="[
+            'group',
+            {
+              'border-b border-white/50': index !== routes.length - 1,
+            },
+          ]"
+        >
+          <RouterLink
+            :to="path"
+            :class="[
+              'flex items-baseline gap-[0.5vw] py-4 leading-none transition-all duration-[400ms] ease-[cubic-bezier([0.83,0,0.17,1])] group-hover:translate-x-4',
+              {
+                'opacity-60': hoveredLink && hoveredLink !== name,
+              },
+            ]"
+          >
+            <span class="text-[1.5vw] italic">{{
+              String(index).padStart(2, '0')
+            }}</span>
+            <span class="text-[4vw] uppercase">{{ linkLabel }}</span>
+          </RouterLink>
+        </li>
+      </template>
+    </ul>
+  </main>
 </template>
