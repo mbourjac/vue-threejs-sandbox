d15baf28f6b7fae70e86842c2f0ee4d3df407a2a
Michaël Bourjac <michael.bourjac@gmail.com>
2024-12-16 07:29:45 +0100

    feat: transform object

--- src/views/ObjectTransformationView.vue
+++ src/views/ObjectTransformationView.vue
@@ -39,6 +39,17 @@ const setupScene = () => {
   const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
   const mesh = new THREE.Mesh(geometry, material);
 
+  mesh.position.x = 0.7;
+  mesh.position.y = -0.6;
+  mesh.position.z = 1;
+
+  mesh.scale.x = 2;
+  mesh.scale.y = 0.25;
+  mesh.scale.z = 0.5;
+
+  mesh.rotation.x = Math.PI * 0.25;
+  mesh.rotation.y = Math.PI * 0.25;
+
   scene.add(mesh);
 
   /**
@@ -46,6 +57,7 @@ const setupScene = () => {
    */
   camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
   camera.position.z = 3;
+  camera.lookAt(mesh.position);
 
   scene.add(camera);
 
