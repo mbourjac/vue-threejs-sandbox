9c5cd6000db71aa949e8e91dd1e9e35936db7cb6
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-03 07:25:25 +0100

    feat: add random toruses

--- src/views/3DTextView.vue
+++ src/views/3DTextView.vue
@@ -25,6 +25,12 @@ useThree({
     const fontLoader = new FontLoader();
 
     fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+      // Material
+      const material = new THREE.MeshMatcapMaterial({
+        matcap: matcapTexture,
+      });
+
+      // Text
       const textGeometry = new TextGeometry('Hello Three.js', {
         font: font,
         size: 0.5,
@@ -39,12 +45,28 @@ useThree({
 
       textGeometry.center();
 
-      const textMaterial = new THREE.MeshMatcapMaterial({
-        matcap: matcapTexture,
-      });
-      const text = new THREE.Mesh(textGeometry, textMaterial);
+      const text = new THREE.Mesh(textGeometry, material);
 
       scene.add(text);
+
+      // Donuts
+      const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+
+      for (let i = 0; i < 100; i++) {
+        const donut = new THREE.Mesh(donutGeometry, material);
+
+        donut.position.x = (Math.random() - 0.5) * 10;
+        donut.position.y = (Math.random() - 0.5) * 10;
+        donut.position.z = (Math.random() - 0.5) * 10;
+
+        donut.rotation.x = Math.random() * Math.PI;
+        donut.rotation.y = Math.random() * Math.PI;
+
+        const scale = Math.random();
+        donut.scale.set(scale, scale, scale);
+
+        scene.add(donut);
+      }
     });
 
     /**
