30345a68edad4b7c6aa49585bd4d3191bf02858f
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-06 18:52:26 +0100

    feat: optimize shadows

--- src/views/ShadowsView.vue
+++ src/views/ShadowsView.vue
@@ -27,6 +27,15 @@ useThree({
 
     directionalLight.position.set(2, 2, -1);
     directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 1024;
+    directionalLight.shadow.mapSize.height = 1024;
+    directionalLight.shadow.camera.near = 1;
+    directionalLight.shadow.camera.far = 6;
+    directionalLight.shadow.camera.top = 2;
+    directionalLight.shadow.camera.right = 2;
+    directionalLight.shadow.camera.bottom = -2;
+    directionalLight.shadow.camera.left = -2;
+    directionalLight.shadow.radius = 10;
 
     gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001);
     gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
@@ -34,6 +43,14 @@ useThree({
     gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
 
     scene.add(directionalLight);
+
+    const directionalLightCameraHelper = new THREE.CameraHelper(
+      directionalLight.shadow.camera
+    );
+
+    directionalLightCameraHelper.visible = false;
+
+    scene.add(directionalLightCameraHelper);
 
     /**
      * Materials
