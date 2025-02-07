97e4922d34a4cb07092bf5104831c5a147bef51f
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-07 07:22:31 +0100

    feat: animate mesh with shadow texture

--- src/views/ShadowsView.vue
+++ src/views/ShadowsView.vue
@@ -20,6 +20,8 @@ useThree({
     const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
     bakedShadow.colorSpace = THREE.SRGBColorSpace;
 
+    const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg');
+
     /**
      * Lights
      */
@@ -121,18 +123,26 @@ useThree({
 
     sphere.castShadow = true;
 
-    const plane = new THREE.Mesh(
-      new THREE.PlaneGeometry(5, 5),
-      new THREE.MeshBasicMaterial({
-        map: bakedShadow,
-      })
-    );
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 
     plane.rotation.x = -Math.PI * 0.5;
     plane.position.y = -0.5;
     plane.receiveShadow = true;
 
     scene.add(sphere, plane);
+
+    const sphereShadow = new THREE.Mesh(
+      new THREE.PlaneGeometry(1.5, 1.5),
+      new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow,
+      })
+    );
+    sphereShadow.rotation.x = -Math.PI * 0.5;
+    sphereShadow.position.y = plane.position.y + 0.01;
+
+    scene.add(sphere, sphereShadow, plane);
 
     /**
      * Camera
@@ -155,6 +165,17 @@ useThree({
       renderer,
       camera,
       controls,
+      tick: (elapsedTime) => {
+        // Update the sphere
+        sphere.position.x = Math.cos(elapsedTime) * 1.5;
+        sphere.position.z = Math.sin(elapsedTime) * 1.5;
+        sphere.position.y = Math.abs(Math.sin(elapsedTime * 3));
+
+        // Update the shadow
+        sphereShadow.position.x = sphere.position.x;
+        sphereShadow.position.z = sphere.position.z;
+        sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3;
+      },
     });
   },
 });
