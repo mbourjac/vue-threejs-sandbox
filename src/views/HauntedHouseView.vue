ec32386b1da2b29aebd806202e75b2a738105c26
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-12 07:19:14 +0100

    feat(haunted-house): add graves texture

--- src/views/HauntedHouseView.vue
+++ src/views/HauntedHouseView.vue
@@ -106,6 +106,23 @@ useThree({
     bushARMTexture.repeat.set(2, 1);
     bushNormalTexture.repeat.set(2, 1);
 
+    // Grave
+    const graveColorTexture = textureLoader.load(
+      './haunted-house/grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.webp'
+    );
+    const graveARMTexture = textureLoader.load(
+      './haunted-house/grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.webp'
+    );
+    const graveNormalTexture = textureLoader.load(
+      './haunted-house/grave/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.webp'
+    );
+
+    graveColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+    graveColorTexture.repeat.set(0.3, 0.4);
+    graveARMTexture.repeat.set(0.3, 0.4);
+    graveNormalTexture.repeat.set(0.3, 0.4);
+
     /**
      * Lights
      */
@@ -242,7 +259,13 @@ useThree({
 
     // Graves
     const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
-    const graveMaterial = new THREE.MeshStandardMaterial();
+    const graveMaterial = new THREE.MeshStandardMaterial({
+      map: graveColorTexture,
+      aoMap: graveARMTexture,
+      roughnessMap: graveARMTexture,
+      metalnessMap: graveARMTexture,
+      normalMap: graveNormalTexture,
+    });
 
     const graves = new THREE.Group();
     scene.add(graves);
