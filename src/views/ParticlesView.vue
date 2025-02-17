aa05b0de31284d988bca6853ed83c55753bd7813
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-17 19:56:46 +0100

    feat(particles): add texture to particles

--- src/views/ParticlesView.vue
+++ src/views/ParticlesView.vue
@@ -10,6 +10,12 @@ useThree({
   useFullScreen: true,
   setupScene: ({ scene, renderer, animate, controls, camera }) => {
     /**
+     * Textures
+     */
+    const textureLoader = new THREE.TextureLoader();
+    const particleTexture = textureLoader.load('/textures/particles/2.png');
+
+    /**
      * Particles
      */
     // Geometry
@@ -17,6 +23,7 @@ useThree({
     const count = 5000;
 
     const positions = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
+    const colors = new Float32Array(count * 3);
 
     for (
       let i = 0;
@@ -24,12 +31,17 @@ useThree({
       i++ // Multiply by 3 for same reason
     ) {
       positions[i] = (Math.random() - 0.5) * 10; // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+      colors[i] = Math.random();
     }
 
     particlesGeometry.setAttribute(
       'position',
       new THREE.BufferAttribute(positions, 3)
     ); // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+    particlesGeometry.setAttribute(
+      'color',
+      new THREE.BufferAttribute(colors, 3)
+    );
 
     // Material
     const particlesMaterial = new THREE.PointsMaterial({
@@ -38,6 +50,16 @@ useThree({
     });
 
     particlesMaterial.size = 0.1;
+    // particlesMaterial.color = new THREE.Color('#ff88cc');
+    particlesMaterial.transparent = true;
+    particlesMaterial.alphaMap = particleTexture;
+
+    // particlesMaterial.alphaTest = 0.001
+    // particlesMaterial.depthTest = false
+    particlesMaterial.depthWrite = false;
+
+    particlesMaterial.blending = THREE.AdditiveBlending;
+    particlesMaterial.vertexColors = true;
 
     // Points
     const particles = new THREE.Points(particlesGeometry, particlesMaterial);
