55234cede0b17688f0e7e22e313951e62cde740d
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-18 19:12:33 +0100

    feat(galaxy-generator): create base particles

--- src/views/GalaxyGeneratorView.vue
+++ src/views/GalaxyGeneratorView.vue
@@ -9,14 +9,52 @@ useThree({
   canvasRef,
   setupScene: ({ scene, renderer, animate, controls, camera }) => {
     /**
-     * Object
+     * Galaxy
      */
-    const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial()
-    );
+    const parameters = {
+      count: 1000,
+      size: 0.02,
+    };
 
-    scene.add(cube);
+    const generateGalaxy = () => {
+      /**
+       * Geometry
+       */
+      const geometry = new THREE.BufferGeometry();
+      const positions = new Float32Array(parameters.count * 3);
+
+      for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3;
+
+        positions[i3] = (Math.random() - 0.5) * 3;
+        positions[i3 + 1] = (Math.random() - 0.5) * 3;
+        positions[i3 + 2] = (Math.random() - 0.5) * 3;
+      }
+
+      geometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+      );
+
+      /**
+       * Material
+       */
+      const material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+      });
+
+      /**
+       * Points
+       */
+      const points = new THREE.Points(geometry, material);
+
+      scene.add(points);
+    };
+
+    generateGalaxy();
 
     /**
      * Camera
