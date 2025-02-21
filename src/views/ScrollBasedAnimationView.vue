4be9a674a4ae82949514ca9fd73a3fe17cd73a61
Michaël Bourjac <michael.bourjac@gmail.com>
2025-02-21 18:15:11 +0100

    feat(scroll-based-animation): create base objects

--- src/views/ScrollBasedAnimationView.vue
+++ src/views/ScrollBasedAnimationView.vue
@@ -10,14 +10,32 @@ useThree({
   useFullScreen: true,
   setupScene: ({ scene, renderer, animate, controls, camera }) => {
     /**
+     * Debug
+     */
+    const parameters = {
+      materialColor: '#ffeded',
+    };
+
+    /**
      * Objects
      */
-    const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ color: '#ff0000' })
+    // Material
+    const material = new THREE.MeshToonMaterial({
+      color: parameters.materialColor,
+    });
+
+    // Meshes
+    const mesh1 = new THREE.Mesh(
+      new THREE.TorusGeometry(1, 0.4, 16, 60),
+      material
+    );
+    const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+    const mesh3 = new THREE.Mesh(
+      new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+      material
     );
 
-    scene.add(cube);
+    scene.add(mesh1, mesh2, mesh3);
 
     /**
      * Camera
