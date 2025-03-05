1611f909ddea5e39b0043677fd140a2eee0f7f5c
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-05 07:12:21 +0100

    feat(physics): add reset controller

--- src/views/PhysicsView.vue
+++ src/views/PhysicsView.vue
@@ -202,10 +202,23 @@ useThree({
           z: (Math.random() - 0.5) * 3,
         });
       },
+      reset: () => {
+        for (const object of objectsToUpdate) {
+          // Remove body
+          object.body.removeEventListener('collide', playHitSound);
+          world.remove(object.body);
+
+          // Remove mesh
+          scene.remove(object.mesh);
+        }
+
+        objectsToUpdate.splice(0, objectsToUpdate.length);
+      },
     };
 
     gui.add(debugObject, 'createSphere');
     gui.add(debugObject, 'createBox');
+    gui.add(debugObject, 'reset');
 
     /**
      * Lights
