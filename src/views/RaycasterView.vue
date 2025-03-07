aa0da6110a94e40471903e804655e8860a983b20
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-07 06:44:58 +0100

    feat(raycaster): animate objects and create raycaster

--- src/views/RaycasterView.vue
+++ src/views/RaycasterView.vue
@@ -33,6 +33,11 @@ useThree({
     scene.add(object1, object2, object3);
 
     /**
+     * Raycaster
+     */
+    const raycaster = new THREE.Raycaster();
+
+    /**
      * Camera
      */
     camera.position.z = 3;
@@ -45,6 +50,32 @@ useThree({
       renderer,
       camera,
       controls,
+      tick: (elapsedTime) => {
+        // Animate objects
+        object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+        object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+        object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+        // Cast a ray
+        const rayOrigin = new THREE.Vector3(-3, 0, 0);
+        const rayDirection = new THREE.Vector3(1, 0, 0);
+
+        rayDirection.normalize();
+        raycaster.set(rayOrigin, rayDirection);
+
+        const objectsToTest = [object1, object2, object3];
+        const intersects = raycaster.intersectObjects(objectsToTest);
+
+        for (const object of objectsToTest) {
+          object.material.color.set('#ff0000');
+        }
+
+        for (const intersect of intersects) {
+          if (intersect.object instanceof THREE.Mesh) {
+            intersect.object.material.color.set('#0000ff');
+          }
+        }
+      },
     });
   },
 });
