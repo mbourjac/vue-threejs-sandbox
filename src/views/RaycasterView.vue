d439f9ee548bb5b74d99d7b507c312e38c594236
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-10 19:10:54 +0100

    feat(raycaster): handle click event with raycaster

--- src/views/RaycasterView.vue
+++ src/views/RaycasterView.vue
@@ -9,13 +9,54 @@ const canvasRef = useTemplateRef('canvas');
 const { width, height } = useWindowSize();
 
 /**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+object3.position.x = 2;
+
+/**
  * Mouse
  */
 const mouse = new THREE.Vector2();
+let currentIntersect: THREE.Intersection | null = null;
 
 const handleMouse = (event: MouseEvent) => {
   mouse.x = (event.clientX / width.value) * 2 - 1;
   mouse.y = -(event.clientY / height.value) * 2 + 1;
+};
+
+const handleClick = () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log('click on object 1');
+        break;
+
+      case object2:
+        console.log('click on object 2');
+        break;
+
+      case object3:
+        console.log('click on object 3');
+        break;
+    }
+  }
 };
 
 useThree({
@@ -24,25 +65,6 @@ useThree({
     /**
      * Objects
      */
-    const object1 = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5, 16, 16),
-      new THREE.MeshBasicMaterial({ color: '#ff0000' })
-    );
-
-    object1.position.x = -2;
-
-    const object2 = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5, 16, 16),
-      new THREE.MeshBasicMaterial({ color: '#ff0000' })
-    );
-
-    const object3 = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5, 16, 16),
-      new THREE.MeshBasicMaterial({ color: '#ff0000' })
-    );
-
-    object3.position.x = 2;
-
     scene.add(object1, object2, object3);
 
     /**
@@ -71,18 +93,21 @@ useThree({
 
         // Cast a ray
         raycaster.setFromCamera(mouse, camera);
-
         const objectsToTest = [object1, object2, object3];
         const intersects = raycaster.intersectObjects(objectsToTest);
 
-        for (const object of objectsToTest) {
-          object.material.color.set('#ff0000');
-        }
-
-        for (const intersect of intersects) {
-          if (intersect.object instanceof THREE.Mesh) {
-            intersect.object.material.color.set('#0000ff');
+        if (intersects.length) {
+          if (!currentIntersect) {
+            console.log('mouse enter');
           }
+
+          currentIntersect = intersects[0];
+        } else {
+          if (currentIntersect) {
+            console.log('mouse leave');
+          }
+
+          currentIntersect = null;
         }
       },
     });
@@ -91,10 +116,12 @@ useThree({
 
 onMounted(() => {
   window.addEventListener('mousemove', handleMouse);
+  window.addEventListener('click', handleClick);
 });
 
 onUnmounted(() => {
   window.removeEventListener('mousemove', handleMouse);
+  window.removeEventListener('click', handleClick);
 });
 </script>
 
