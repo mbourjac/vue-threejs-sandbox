9b584d9ec3816822190a1f4133bb9f3264929377
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-17 19:08:03 +0100

    feat(realistic-render): add base with texture

--- src/views/RealisticRenderView.vue
+++ src/views/RealisticRenderView.vue
@@ -19,6 +19,7 @@ useThree({
     /**
      * Loaders
      */
+    const textureLoader = new THREE.TextureLoader();
     const gltfLoader = new GLTFLoader();
     const rgbeLoader = new RGBELoader();
 
@@ -56,6 +57,65 @@ useThree({
 
       updateAllMaterials();
     });
+
+    /**
+     * Floor
+     */
+    const floorColorTexture = textureLoader.load(
+      '/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_diff_1k.jpg'
+    );
+    const floorNormalTexture = textureLoader.load(
+      '/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_nor_gl_1k.png'
+    );
+    const floorAORoughnessMetalnessTexture = textureLoader.load(
+      '/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_arm_1k.jpg'
+    );
+
+    const floor = new THREE.Mesh(
+      new THREE.PlaneGeometry(8, 8),
+      new THREE.MeshStandardMaterial({
+        map: floorColorTexture,
+        normalMap: floorNormalTexture,
+        aoMap: floorAORoughnessMetalnessTexture,
+        roughnessMap: floorAORoughnessMetalnessTexture,
+        metalnessMap: floorAORoughnessMetalnessTexture,
+      })
+    );
+
+    floor.rotation.x = -Math.PI * 0.5;
+    floorColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+    scene.add(floor);
+
+    /**
+     * Wall
+     */
+    const wallColorTexture = textureLoader.load(
+      '/textures/castle_brick_broken_06/castle_brick_broken_06_diff_1k.jpg'
+    );
+    const wallNormalTexture = textureLoader.load(
+      '/textures/castle_brick_broken_06/castle_brick_broken_06_nor_gl_1k.png'
+    );
+    const wallAORoughnessMetalnessTexture = textureLoader.load(
+      '/textures/castle_brick_broken_06/castle_brick_broken_06_arm_1k.jpg'
+    );
+
+    const wall = new THREE.Mesh(
+      new THREE.PlaneGeometry(8, 8),
+      new THREE.MeshStandardMaterial({
+        map: wallColorTexture,
+        normalMap: wallNormalTexture,
+        aoMap: wallAORoughnessMetalnessTexture,
+        roughnessMap: wallAORoughnessMetalnessTexture,
+        metalnessMap: wallAORoughnessMetalnessTexture,
+      })
+    );
+
+    wall.position.y = 4;
+    wall.position.z = -4;
+    wallColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+    scene.add(wall);
 
     /**
      * Directional light
