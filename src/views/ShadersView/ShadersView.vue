77796a9e8464aa50c0885c7c63922e8a8fc5f729
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-19 07:15:20 +0100

    refactor(shaders): move the shaders into their own files

--- src/views/ShadersView/ShadersView.vue
+++ src/views/ShadersView/ShadersView.vue
@@ -2,6 +2,8 @@
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
 import { useThree } from '@/composables/use-three';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 const canvasRef = useTemplateRef('canvas');
 
@@ -13,26 +15,8 @@ useThree({
      */
     const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
     const material = new THREE.RawShaderMaterial({
-      vertexShader: `
-        uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
-
-        attribute vec3 position;
-
-        void main()
-        {
-            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-        }
-    `,
-      fragmentShader: `
-        precision mediump float;
-
-        void main()
-        {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    `,
+      vertexShader,
+      fragmentShader,
     });
     const mesh = new THREE.Mesh(geometry, material);
 
