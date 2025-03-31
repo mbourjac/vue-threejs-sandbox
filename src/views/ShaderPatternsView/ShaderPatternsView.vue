41287f55808b199144417c328f757364818ee016
Michaël Bourjac <michael.bourjac@gmail.com>
2025-03-31 19:02:28 +0200

    feat(shader-patterns): create steep  gradient pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/inverted-gradient/vertex.glsl';
-import fragmentShader from './shaders/inverted-gradient/fragment.glsl';
+import vertexShader from './shaders/steep-gradient/vertex.glsl';
+import fragmentShader from './shaders/steep-gradient/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
