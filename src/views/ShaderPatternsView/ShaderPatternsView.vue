26eb9a3167cc8f6a04c81149d12d313fcf7f4088
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-01 19:35:53 +0200

    feat(shader-patterns): create lines pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/steps/vertex.glsl';
-import fragmentShader from './shaders/steps/fragment.glsl';
+import vertexShader from './shaders/lines/vertex.glsl';
+import fragmentShader from './shaders/lines/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
