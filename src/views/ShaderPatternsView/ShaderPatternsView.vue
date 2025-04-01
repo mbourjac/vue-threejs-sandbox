5a809faeecc3f00f4bb48ebe9fbac8c8414c702b
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-01 19:32:39 +0200

    feat(shader-patterns): create repeated gradient pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/steep-gradient/vertex.glsl';
-import fragmentShader from './shaders/steep-gradient/fragment.glsl';
+import vertexShader from './shaders/repeated-gradient/vertex.glsl';
+import fragmentShader from './shaders/repeated-gradient/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
