2bb397968fa8c52efea178b34a95daf0776d8dca
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-02 19:30:57 +0200

    feat(shader-patterns): create grid pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/lines/vertex.glsl';
-import fragmentShader from './shaders/lines/fragment.glsl';
+import vertexShader from './shaders/grid/vertex.glsl';
+import fragmentShader from './shaders/grid/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
