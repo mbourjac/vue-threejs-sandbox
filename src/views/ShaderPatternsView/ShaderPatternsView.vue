e39281b6869139550b483911e28ab354b7aa4c35
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-02 19:32:40 +0200

    feat(shader-patterns): create grid intersections pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,45 +1,67 @@
 <script setup lang="ts">
-import { useTemplateRef } from 'vue';
+import { onMounted, useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/grid/vertex.glsl';
-import fragmentShader from './shaders/grid/fragment.glsl';
-import { useThree } from '@/composables/use-three';
+import { useAnimateScene } from '../../composables/use-animate-scene';
+import { useSizes } from '../../composables/use-sizes';
+import { useResizeScene } from '../../composables/use-resize-scene';
+import { useOrbitControls } from '../../composables/use-orbit-controls';
+import { useScene } from '../../composables/use-scene';
+import vertexShader from './shaders/grid-intersections/vertex.glsl';
+import fragmentShader from './shaders/grid-intersections/fragment.glsl';
 
 const canvasRef = useTemplateRef('canvas');
 
-useThree({
-  canvasRef,
-  setupScene: ({ scene, renderer, animate, controls, camera }) => {
-    /**
-     * Object
-     */
-    const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+const { width, height, aspectRatio } = useSizes();
+const { scene, setupRenderer } = useScene();
+const { animate } = useAnimateScene();
+const { setupOrbitControls } = useOrbitControls();
 
-    const material = new THREE.ShaderMaterial({
-      vertexShader,
-      fragmentShader,
-      side: THREE.DoubleSide,
-    });
+const setupScene = () => {
+  const canvas = canvasRef.value;
 
-    const mesh = new THREE.Mesh(geometry, material);
+  if (!canvas) return;
 
-    scene.add(mesh);
+  /**
+   * Object
+   */
+  const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
-    /**
-     * Camera
-     */
-    camera.position.set(0.25, -0.25, 1);
+  const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    side: THREE.DoubleSide,
+  });
 
-    /**
-     * Animate
-     */
-    animate({
-      scene,
-      renderer,
-      camera,
-      controls,
-    });
-  },
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, aspectRatio.value, 0.1, 100);
+
+  camera.position.set(0.25, -0.25, 1);
+  scene.add(camera);
+
+  // Controls
+  const controls = setupOrbitControls(camera, canvas);
+
+  // Renderer
+  const { renderer } = setupRenderer(canvas, { width, height });
+
+  // Resize
+  useResizeScene({ camera, renderer, sizes: { width, height } });
+
+  // Animate
+  animate({
+    scene,
+    renderer,
+    camera,
+    controls,
+  });
+};
+
+onMounted(() => {
+  setupScene();
 });
 </script>
 
