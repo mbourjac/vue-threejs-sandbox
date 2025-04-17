de80878df59fb8795ec9982fc010fbd1f761a2cd
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-17 20:03:53 +0200

    feat(shader-patterns): create normalized angular gradient pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/angular-gradient/vertex.glsl';
-import fragmentShader from './shaders/angular-gradient/fragment.glsl';
+import vertexShader from './shaders/normalized-angular-gradient/vertex.glsl';
+import fragmentShader from './shaders/normalized-angular-gradient/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
