4a22382daa576a06c7ff0ac57825d5c6a2c6a6b9
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-23 19:35:15 +0200

    feat(shader-patterns): create dash pattern

--- src/views/ShaderPatternsView/ShaderPatternsView.vue
+++ src/views/ShaderPatternsView/ShaderPatternsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import * as THREE from 'three';
-import vertexShader from './shaders/stepped-perlin-noise/vertex.glsl';
-import fragmentShader from './shaders/stepped-perlin-noise/fragment.glsl';
+import vertexShader from './shaders/dashes/vertex.glsl';
+import fragmentShader from './shaders/dashes/fragment.glsl';
 import { useThree } from '@/composables/use-three';
 
 const canvasRef = useTemplateRef('canvas');
