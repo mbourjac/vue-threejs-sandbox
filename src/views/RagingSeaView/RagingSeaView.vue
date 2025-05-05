3437f4bfa9ccf40798beff2ef997470fe2ad30fc
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-05 19:28:02 +0200

    feat(raging-sea): create uniform for big waves frequency

--- src/views/RagingSeaView/RagingSeaView.vue
+++ src/views/RagingSeaView/RagingSeaView.vue
@@ -25,6 +25,7 @@ useThree({
       fragmentShader: waterFragmentShader,
       uniforms: {
         uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
       },
     });
 
@@ -34,6 +35,18 @@ useThree({
       .max(1)
       .step(0.001)
       .name('uBigWavesElevation');
+    gui
+      .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .name('uBigWavesFrequencyX');
+    gui
+      .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .name('uBigWavesFrequencyY');
 
     // Mesh
     const water = new THREE.Mesh(waterGeometry, waterMaterial);
