66336c0a2f81e631c21ba538d8033a81f654b4be
Michaël Bourjac <michael.bourjac@gmail.com>
2025-05-13 20:28:24 +0200

    feat(animated-galaxy): implement diffuse point pattern on particles

--- src/views/AnimatedGalaxyView/shaders/galaxy/fragment.glsl
+++ src/views/AnimatedGalaxyView/shaders/galaxy/fragment.glsl
@@ -1,7 +1,7 @@
 void main() {
-  // Disc
+  // Diffuse point
   float strength = distance(gl_PointCoord, vec2(0.5));
-  strength = step(0.5, strength);
+  strength *= 2.0;
   strength = 1.0 - strength;
 
   gl_FragColor = vec4(vec3(strength), 1.0);
