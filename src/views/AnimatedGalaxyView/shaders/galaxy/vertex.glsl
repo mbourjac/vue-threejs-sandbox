96e7484d887fbe87aca877fc5d1d301e1c6d2a72
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-03 19:29:07 +0200

    feat(animated-galaxy): handle colors in shaders

--- src/views/AnimatedGalaxyView/shaders/galaxy/vertex.glsl
+++ src/views/AnimatedGalaxyView/shaders/galaxy/vertex.glsl
@@ -2,18 +2,19 @@ uniform float uSize;
 
 attribute float aScale;
 
+varying vec3 vColor;
+
 void main() {
-  /**
-    * Position
-    */
+  // Position
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
   gl_Position = projectedPosition;
 
-  /**
-    * Size
-    */
+  // Size
   gl_PointSize = uSize * aScale;
   gl_PointSize *= (1.0 / -viewPosition.z);
+
+  // Varyings
+  vColor = color;
 }
