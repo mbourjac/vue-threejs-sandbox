1562dab673c11c75389eefbce0588dbd44115d87
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-04 20:01:38 +0200

    feat(modified-materials): twist vertices

--- src/views/ModifiedMaterialsView/ModifiedMaterialsView.vue
+++ src/views/ModifiedMaterialsView/ModifiedMaterialsView.vue
@@ -51,9 +51,36 @@ useThree({
       normalMap: normalTexture,
     });
 
+    material.onBeforeCompile = (shader) => {
+      shader.vertexShader = shader.vertexShader.replace(
+        '#include <common>',
+        `
+        #include <common>
+
+        mat2 get2dRotateMatrix(float _angle)
+        {
+            return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+        }
+      `
+      );
+
+      shader.vertexShader = shader.vertexShader.replace(
+        '#include <begin_vertex>',
+        `
+        #include <begin_vertex>
+
+        float angle = position.y * 0.9;
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+        transformed.xz = rotateMatrix * transformed.xz;
+      `
+      );
+    };
+
     /**
      * Models
      */
+
     const updateAllMaterials = () => {
       scene.traverse((child) => {
         if (
