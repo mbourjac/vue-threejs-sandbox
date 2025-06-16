cbb33a2d3b446551444b066492697139dec09ced
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-16 19:22:48 +0200

    feat(hologram): create glitch effect

--- src/views/HologramView/shaders/hologram/vertex.glsl
+++ src/views/HologramView/shaders/hologram/vertex.glsl
@@ -1,5 +1,9 @@
+uniform float uTime;
+
 varying vec3 vPosition;
 varying vec3 vNormal;
+
+#include ../../../../utils/glsl/random-2d.glsl
 
 void main() {
   // Position
@@ -7,6 +11,16 @@ void main() {
 
   // Model normal
   vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
+  // Glitch   
+  float glitchTime = uTime - modelPosition.y;
+  float glitchStrength = sin(glitchTime) + sin(glitchTime * 2.34) + sin(glitchTime * 5.67);
+  glitchStrength /= 3.0;
+  glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
+  glitchStrength *= 0.25;
+
+  modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
+  modelPosition.z += (random2D(modelPosition.zx + uTime) - 0.5) * glitchStrength;
 
   // Final position
   gl_Position = projectionMatrix * viewMatrix * modelPosition;
