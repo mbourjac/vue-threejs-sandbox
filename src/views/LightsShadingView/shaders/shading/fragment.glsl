fd8398dcec2122720caebb908f99d73a1fd10ee4
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-27 19:03:23 +0200

    feat(lights-shading): create ambient light shader

--- src/views/LightsShadingView/shaders/shading/fragment.glsl
+++ src/views/LightsShadingView/shaders/shading/fragment.glsl
@@ -1,7 +1,14 @@
+#include ../../../../utils/glsl/ambient-light.glsl
+
 uniform vec3 uColor;
 
 void main() {
   vec3 color = uColor;
+
+  // Lights
+  vec3 light = vec3(0.0);
+  light += ambientLight(vec3(1.0), 0.03);
+  color *= light;
 
   // Final color
   gl_FragColor = vec4(color, 1.0);
