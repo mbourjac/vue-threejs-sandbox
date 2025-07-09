bc5f449120f842a078e4a08ddb8cbcc9538e77e4
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-09 23:04:42 +0200

    feat(halftone-shading): create points grid

--- src/views/HalftoneShadingView/shaders/halftone/fragment.glsl
+++ src/views/HalftoneShadingView/shaders/halftone/fragment.glsl
@@ -22,13 +22,22 @@ void main() {
 
 	// Halftone
   float repetitions = 50.0;
+  vec3 direction = vec3(0.0, -1.0, 0.0);
+  float low = -0.8;
+  float high = 1.5;
+
+  float intensity = dot(normal, direction);
+  intensity = smoothstep(low, high, intensity);
 
   vec2 uv = gl_FragCoord.xy / uResolution.y;
   uv *= repetitions;
   uv = mod(uv, 1.0);
 
+  float point = distance(uv, vec2(0.5));
+  point = 1.0 - step(0.5 * intensity, point);
+
 	// Final color
-  gl_FragColor = vec4(uv, 1.0, 1.0);
+  gl_FragColor = vec4(point, point, point, 1.0);
 
 	#include <tonemapping_fragment>
 	#include <colorspace_fragment>
