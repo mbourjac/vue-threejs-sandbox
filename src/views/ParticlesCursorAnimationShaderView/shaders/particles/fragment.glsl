47f2a3fad935fbe49ec4f1304a63687b13bd2d5f
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-15 19:46:14 +0200

    feat(particles-cursor-animation): shape particles as discs

--- src/views/ParticlesCursorAnimationShaderView/shaders/particles/fragment.glsl
+++ src/views/ParticlesCursorAnimationShaderView/shaders/particles/fragment.glsl
@@ -1,4 +1,10 @@
 void main() {
+  vec2 uv = gl_PointCoord;
+  float distanceToCenter = distance(uv, vec2(0.5));
+
+  if(distanceToCenter > 0.5)
+    discard;
+
   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 
   #include <tonemapping_fragment>
