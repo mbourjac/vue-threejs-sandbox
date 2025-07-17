c74f54e421f1c85828e0be256b32a3b80a18dc2e
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-17 20:22:25 +0200

    feat(particles-cursor-animation): randomize particles movement angle

--- src/views/ParticlesCursorAnimationShaderView/shaders/particles/vertex.glsl
+++ src/views/ParticlesCursorAnimationShaderView/shaders/particles/vertex.glsl
@@ -3,6 +3,7 @@ uniform sampler2D uPictureTexture;
 uniform sampler2D uDisplacementTexture;
 
 attribute float aIntensity;
+attribute float aAngle;
 
 varying vec3 vColor;
 
@@ -11,8 +12,9 @@ void main() {
   vec3 newPosition = position;
   float displacementIntensity = texture(uDisplacementTexture, uv).r;
 
-  vec3 displacement = vec3(0.0, 0.0, 1.0);
+  vec3 displacement = vec3(cos(aAngle) * 0.2, sin(aAngle) * 0.2, 1.0);
 
+  displacement = normalize(displacement);
   displacement *= displacementIntensity;
   displacement *= 3.0;
   displacement *= aIntensity;
