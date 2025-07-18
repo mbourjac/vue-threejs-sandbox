0526a3b6355085a6835bb4988adde720c8621db6
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-18 18:23:30 +0200

    fix(particles-cursor-animation): allow particles to come back to their position

--- src/views/ParticlesCursorAnimationShaderView/shaders/particles/vertex.glsl
+++ src/views/ParticlesCursorAnimationShaderView/shaders/particles/vertex.glsl
@@ -11,6 +11,7 @@ void main() {
   // Displacement
   vec3 newPosition = position;
   float displacementIntensity = texture(uDisplacementTexture, uv).r;
+  displacementIntensity = smoothstep(0.1, 1.0, displacementIntensity);
 
   vec3 displacement = vec3(cos(aAngle) * 0.2, sin(aAngle) * 0.2, 1.0);
 
