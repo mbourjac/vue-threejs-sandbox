58322711fc33fced899ea9bee5ffa3de81cade1e
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-23 19:17:02 +0200

    feat(particles-morphing): improve transition

--- src/views/ParticlesMorphingShaderView/shaders/particles/vertex.glsl
+++ src/views/ParticlesMorphingShaderView/shaders/particles/vertex.glsl
@@ -1,12 +1,24 @@
+#include ../../../../utils/glsl/simplex-noise-3d.glsl
+
 uniform vec2 uResolution;
 uniform float uSize;
 uniform float uProgress;
 
 attribute vec3 aPositionTarget;
 
+varying vec3 vColor;
+
 void main() {
   // Mixed position
-  float progress = uProgress;
+  float noiseOrigin = simplexNoise3d(position * 0.2);
+  float noiseTarget = simplexNoise3d(aPositionTarget * 0.2);
+  float noise = mix(noiseOrigin, noiseTarget, uProgress);
+  noise = smoothstep(-1.0, 1.0, noise);
+
+  float duration = 0.4;
+  float delay = (1.0 - duration) * noise;
+  float end = delay + duration;
+  float progress = smoothstep(delay, end, uProgress);
   vec3 mixedPosition = mix(position, aPositionTarget, progress);
 
   // Final position
@@ -18,4 +30,7 @@ void main() {
   // Point size
   gl_PointSize = uSize * uResolution.y;
   gl_PointSize *= (1.0 / -viewPosition.z);
+
+  // Varyings
+  vColor = vec3(noise);
 }
