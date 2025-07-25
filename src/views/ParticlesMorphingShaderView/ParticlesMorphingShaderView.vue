5bc98bc086a51841b05d76658021509ce7503eea
Michaël Bourjac <michael.bourjac@gmail.com>
2025-07-25 18:40:43 +0200

    refactor(particles-morphing): create particles class

--- src/views/ParticlesMorphingShaderView/ParticlesMorphingShaderView.vue
+++ src/views/ParticlesMorphingShaderView/ParticlesMorphingShaderView.vue
@@ -42,148 +42,165 @@ useThree({
       /**
        * Particles
        */
-      let particlesIndex = 0;
+      class Particles {
+        geometry: THREE.BufferGeometry;
+        material: THREE.ShaderMaterial;
+        points: THREE.Points;
+        index: number;
+        primaryColor: string;
+        secondaryColor: string;
+        maxCount: number;
+        morph0: () => void;
+        morph1: () => void;
+        morph2: () => void;
+        morph3: () => void;
 
-      // Debug
-      const debug = {
-        primaryColor: '#ff7300',
-        secondaryColor: '#0091ff',
-        morph0: () => {
-          morph(0);
-        },
-        morph1: () => {
-          morph(1);
-        },
-        morph2: () => {
-          morph(2);
-        },
-        morph3: () => {
-          morph(3);
-        },
-      };
+        constructor() {
+          this.index = 0;
 
-      // Positions
-      const positions = gltf.scene.children
-        .filter((child): child is THREE.Mesh => child instanceof THREE.Mesh)
-        .map((child) => child.geometry.attributes.position);
+          this.primaryColor = '#ff7300';
+          this.secondaryColor = '#0091ff';
 
-      let particlesMaxCount = 0;
+          // Positions
+          const positions = gltf.scene.children
+            .filter((child): child is THREE.Mesh => child instanceof THREE.Mesh)
+            .map((child) => child.geometry.attributes.position);
 
-      for (const position of positions) {
-        if (position.count > particlesMaxCount)
-          particlesMaxCount = position.count;
-      }
+          this.maxCount = 0;
 
-      const particlesPositions: THREE.Float32BufferAttribute[] = [];
-
-      for (const position of positions) {
-        const originalArray = position.array;
-        const newArray = new Float32Array(particlesMaxCount * 3);
-
-        for (let i = 0; i < particlesMaxCount; i++) {
-          const i3 = i * 3;
-
-          if (i3 < originalArray.length) {
-            newArray[i3 + 0] = originalArray[i3 + 0];
-            newArray[i3 + 1] = originalArray[i3 + 1];
-            newArray[i3 + 2] = originalArray[i3 + 2];
-          } else {
-            const randomIndex = Math.floor(position.count * Math.random()) * 3;
-
-            newArray[i3 + 0] = originalArray[randomIndex + 0];
-            newArray[i3 + 1] = originalArray[randomIndex + 1];
-            newArray[i3 + 2] = originalArray[randomIndex + 2];
+          for (const position of positions) {
+            if (position.count > this.maxCount) this.maxCount = position.count;
           }
+
+          const particlesPositions: THREE.Float32BufferAttribute[] = [];
+
+          for (const position of positions) {
+            const originalArray = position.array;
+            const newArray = new Float32Array(this.maxCount * 3);
+
+            for (let i = 0; i < this.maxCount; i++) {
+              const i3 = i * 3;
+
+              if (i3 < originalArray.length) {
+                newArray[i3 + 0] = originalArray[i3 + 0];
+                newArray[i3 + 1] = originalArray[i3 + 1];
+                newArray[i3 + 2] = originalArray[i3 + 2];
+              } else {
+                const randomIndex =
+                  Math.floor(position.count * Math.random()) * 3;
+
+                newArray[i3 + 0] = originalArray[randomIndex + 0];
+                newArray[i3 + 1] = originalArray[randomIndex + 1];
+                newArray[i3 + 2] = originalArray[randomIndex + 2];
+              }
+            }
+
+            particlesPositions.push(
+              new THREE.Float32BufferAttribute(newArray, 3)
+            );
+          }
+
+          // Geometry
+          const sizesArray = new Float32Array(this.maxCount);
+
+          for (let i = 0; i < this.maxCount; i++) {
+            sizesArray[i] = Math.random();
+          }
+
+          this.geometry = new THREE.BufferGeometry();
+
+          this.geometry.setAttribute(
+            'position',
+            particlesPositions[this.index]
+          );
+          this.geometry.setAttribute('aPositionTarget', particlesPositions[2]);
+          this.geometry.setAttribute(
+            'aSize',
+            new THREE.BufferAttribute(sizesArray, 1)
+          );
+
+          // Material
+          this.material = new THREE.ShaderMaterial({
+            vertexShader: particlesVertexShader,
+            fragmentShader: particlesFragmentShader,
+            blending: THREE.AdditiveBlending,
+            depthWrite: false,
+            uniforms: {
+              uSize: new THREE.Uniform(0.4),
+              uResolution: new THREE.Uniform(resolution.value),
+              uProgress: new THREE.Uniform(0),
+              uPrimaryColor: new THREE.Uniform(
+                new THREE.Color(this.primaryColor)
+              ),
+              uSecondaryColor: new THREE.Uniform(
+                new THREE.Color(this.secondaryColor)
+              ),
+            },
+          });
+
+          // Points
+          this.points = new THREE.Points(this.geometry, this.material);
+          this.points.frustumCulled = false;
+
+          // Methods
+          const morph = (index: number) => {
+            // Update attributes
+            this.geometry.attributes.position = particlesPositions[this.index];
+            this.geometry.attributes.aPositionTarget =
+              particlesPositions[index];
+
+            // Animate uProgress
+            gsap.fromTo(
+              this.material.uniforms.uProgress,
+              { value: 0 },
+              { value: 1, duration: 3, ease: 'linear' }
+            );
+
+            // Save index
+            this.index = index;
+          };
+
+          this.morph0 = () => {
+            morph(0);
+          };
+          this.morph1 = () => {
+            morph(1);
+          };
+          this.morph2 = () => {
+            morph(2);
+          };
+          this.morph3 = () => {
+            morph(3);
+          };
         }
-
-        particlesPositions.push(new THREE.Float32BufferAttribute(newArray, 3));
       }
 
-      // Geometry
-      const sizesArray = new Float32Array(particlesMaxCount);
+      const particles = new Particles();
 
-      for (let i = 0; i < particlesMaxCount; i++) {
-        sizesArray[i] = Math.random();
-      }
+      scene.add(particles.points);
 
-      const particlesGeometry = new THREE.BufferGeometry();
-
-      particlesGeometry.setAttribute(
-        'position',
-        particlesPositions[particlesIndex]
-      );
-      particlesGeometry.setAttribute('aPositionTarget', particlesPositions[2]);
-      particlesGeometry.setAttribute(
-        'aSize',
-        new THREE.BufferAttribute(sizesArray, 1)
-      );
-
-      // Material
-      const particlesMaterial = new THREE.ShaderMaterial({
-        vertexShader: particlesVertexShader,
-        fragmentShader: particlesFragmentShader,
-        blending: THREE.AdditiveBlending,
-        depthWrite: false,
-        uniforms: {
-          uSize: new THREE.Uniform(0.4),
-          uResolution: new THREE.Uniform(resolution.value),
-          uProgress: new THREE.Uniform(0),
-          uPrimaryColor: new THREE.Uniform(new THREE.Color(debug.primaryColor)),
-          uSecondaryColor: new THREE.Uniform(
-            new THREE.Color(debug.secondaryColor)
-          ),
-        },
-      });
-
-      // Points
-      const particlesPoints = new THREE.Points(
-        particlesGeometry,
-        particlesMaterial
-      );
-
-      particlesPoints.frustumCulled = false;
-
-      scene.add(particlesPoints);
-
-      // Methods
-      const morph = (index: number) => {
-        // Update attributes
-        particlesGeometry.attributes.position =
-          particlesPositions[particlesIndex];
-        particlesGeometry.attributes.aPositionTarget =
-          particlesPositions[index];
-
-        // Animate uProgress
-        gsap.fromTo(
-          particlesMaterial.uniforms.uProgress,
-          { value: 0 },
-          { value: 1, duration: 3, ease: 'linear' }
-        );
-
-        // Save index
-        particlesIndex = index;
-      };
-
-      // Tweaks
+      // Controllers
       gui
-        .add(particlesMaterial.uniforms.uProgress, 'value')
+        .add(particles.material.uniforms.uProgress, 'value')
         .min(0)
         .max(1)
         .step(0.001)
         .name('uProgress')
         .listen();
 
-      gui.add(debug, 'morph0');
-      gui.add(debug, 'morph1');
-      gui.add(debug, 'morph2');
-      gui.add(debug, 'morph3');
+      gui.add(particles, 'morph0');
+      gui.add(particles, 'morph1');
+      gui.add(particles, 'morph2');
+      gui.add(particles, 'morph3');
 
-      gui.addColor(debug, 'primaryColor').onChange(() => {
-        particlesMaterial.uniforms.uPrimaryClor.value.set(debug.primaryColor);
+      gui.addColor(particles, 'primaryColor').onChange(() => {
+        particles.material.uniforms.uPrimaryClor.value.set(
+          particles.primaryColor
+        );
       });
-      gui.addColor(debug, 'secondaryColor').onChange(() => {
-        particlesMaterial.uniforms.uSecondaryColor.value.set(
-          debug.secondaryColor
+      gui.addColor(particles, 'secondaryColor').onChange(() => {
+        particles.material.uniforms.uSecondaryColor.value.set(
+          particles.secondaryColor
         );
       });
     });
