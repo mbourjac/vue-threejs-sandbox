25021f8661d8e37a8e09eb95d9a667b163077611
Michaël Bourjac <michael.bourjac@gmail.com>
2025-08-01 20:52:02 +0200

    feat(gpgpu-flow-field-particles): implement basic wobble

--- src/views/WobblySphereView/shaders/wobble/vertex.glsl
+++ src/views/WobblySphereView/shaders/wobble/vertex.glsl
@@ -1,2 +1,8 @@
+#include ../../../../utils/glsl/simplex-noise-4d.glsl
+
 void main() {
+  // Wobble
+  float wobble = simplexNoise4d(vec4(csm_Position, 0.0));
+
+  csm_Position += wobble * normal;
 }
