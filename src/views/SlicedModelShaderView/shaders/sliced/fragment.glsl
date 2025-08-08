fb63af925dd6f213b639b962fed9c80884b538fa
Michaël Bourjac <michael.bourjac@gmail.com>
2025-08-08 18:12:09 +0200

    feat(sliced-model): create slice

--- src/views/SlicedModelShaderView/shaders/sliced/fragment.glsl
+++ src/views/SlicedModelShaderView/shaders/sliced/fragment.glsl
@@ -1,3 +1,13 @@
+varying vec3 vPosition;
+
 void main() {
-  csm_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  float uSliceStart = 1.0;
+  float uSliceArc = 1.5;
+
+  float angle = atan(vPosition.y, vPosition.x);
+
+  if(angle > uSliceStart && angle < uSliceStart + uSliceArc)
+    discard;
+
+  csm_FragColor = vec4(vec3(angle), 1.0);
 }
--- src/views/SlicedModelShaderView/shaders/sliced/vertex.glsl
+++ src/views/SlicedModelShaderView/shaders/sliced/vertex.glsl
@@ -1,2 +1,5 @@
+varying vec3 vPosition;
+
 void main() {
+  vPosition = csm_Position.xyz;
 }
